34cf5938ca77e6d357920ad0c8168a237df33758
Makar Popov <makar.popov01@mail.ru>
2025-11-05 19:25:06 +0300

    add scanner packages

--- 00-packages.sh
+++ 00-packages.sh
@@ -18,7 +18,7 @@ sudo apt install -y \
     net-tools ca-certificates curl wget apt-transport-https \
     build-essential git ssh tree diffutils \
     ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick \
-    cups cups-client cups-bsd system-config-printer \
+    cups cups-client cups-bsd system-config-printer simple-scan \
     qmk python3-dev python3-venv pipx
 
 sudo install -m 0755 -d /etc/apt/keyrings
