0ad97e41729b926dba0a7f1a76ba099ea379b203
Makar Popov <makar.popov01@mail.ru>
2025-12-15 13:30:34 +0300

    add qmk repo checkout

--- post-install.sh
+++ post-install.sh
@@ -28,6 +28,7 @@ git clone git@github.com:SegFaulti4/qmk_firmware.git $HOME/qmk_firmware && \
 git --git-dir=$HOME/qmk_firmware/.git remote add upstream git@github.com:qmk/qmk_firmware
 sudo cp $HOME/qmk_firmware/util/udev/50-qmk.rules /etc/udev/rules.d/
 qmk setup -y || true
+git --git-dir=$HOME/qmk_firmware/.git checkout current
 
 # install messenger
 curl https://td.telegram.org/tlinux/tsetup.6.1.3.tar.xz --output tsetup.tar.xz && \
