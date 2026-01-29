64cf0aad32c59016a14346189264ee2562c55564
Makar Popov <makar.popov01@mail.ru>
2026-01-29 13:49:33 +0300

    add nm openvpn

--- 04-network.sh
+++ 04-network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 
-sudo apt install -y systemd-resolved openvpn3-client openvpn-systemd-resolved network-manager network-manager-gnome
+sudo apt install -y systemd-resolved openvpn3-client openvpn-systemd-resolved network-manager network-manager-gnome network-manager-openpvn
 sudo openvpn3-admin netcfg-service --config-set systemd-resolved true
