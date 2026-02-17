585510e669d6f9d5d22a7b473dba418f06beed1c
Makar Popov <makar.popov01@mail.ru>
2026-02-17 15:09:39 +0300

    add missing nm packages

--- 04-network.sh
+++ 04-network.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
 
-sudo apt install -y systemd-resolved openvpn3-client openvpn-systemd-resolved network-manager network-manager-gnome network-manager-openpvn
+sudo apt install -y systemd-resolved openvpn3-client openvpn-systemd-resolved \
+  network-manager network-manager-gnome network-manager-openpvn network-manager-openpvn-gnome network-manager-ssh network-manager-ssh-gnome
 sudo openvpn3-admin netcfg-service --config-set systemd-resolved true
