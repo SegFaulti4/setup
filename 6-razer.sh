1386bbbc6576a4ca404b6b4448c80fb39e689219
Makar Popov <makar.popov01@mail.ru>
2026-04-03 17:22:19 +0300

    add razergenie install

--- 6-razer.sh
+++ 6-razer.sh
@@ -7,6 +7,14 @@ cd $HOME/clones/openrazer
 ./scripts/build_debs.sh
 sudo apt install ./dist/*.deb
 
+# start openrazer daemon
 systemctl --user start openrazer-daemon.service
 
+# install razer cli
 pipx install --system-site-packages razer-cli
+
+# install razergenie
+echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg > /dev/null
+sudo apt update
+sudo apt install razergenie
